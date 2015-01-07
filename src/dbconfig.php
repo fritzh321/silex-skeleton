d553b677722b2b9e2f86d0ec4d373669e9c268d7
Fritz Hoste <hoste.fritz@gmail.com>
2015-01-07 14:38:29 +0100

    Update dbconfig.php

--- src/dbconfig.php
+++ src/dbconfig.php
@@ -5,10 +5,10 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
         'dbs.options' => array(
             'db' => array(
                 'driver'   => 'pdo_mysql',
-                'dbname'   => 'admin_silex',
-                'host'     => '185.18.148.35',
-                'user'     => 'admin_silexad',
-                'password' => 'UDve3YeSRB',
+                'dbname'   => 'DB_NAME',
+                'host'     => 'host',
+                'user'     => 'user',
+                'password' => 'PASS',
                 'charset'  => 'utf8',
             ),
         )
